b641b036986d61c9887c40b2d54c27677a1a48eb
rummanH <jah.hasant@gmail.com>
2023-01-19 13:17:51 +0600

    complete section 5 mastering pipes

--- 5-MasteringPipe/pipes/src/app/app.component.ts
+++ 5-MasteringPipe/pipes/src/app/app.component.ts
@@ -7,8 +7,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipes';
-  name : string = ""
+  name : string = "";
+  date : string = "";
+  amount:number = 0;
+  height =0;
+  miles = 0;
+  miles2 =0;
+  car ={
+    make:"Toyota",
+    model:"Camry",
+    year:2000
+  }
+  onMilesChange(event:any){
+    this.miles= event.target.value;
+  }
+  onMiles2Change(event:any){
+    this.miles2= event.target.value;
+  }
+  onDateChange(event:any){
+    this.date = event.target.value;
+  }
   onNameChange(event: any){
     this.name = event.target.value
+  }
+  onAmountChange(event:any){
+    this.amount = parseFloat(event.target.value)
+  }
+  onHeightChange(event:any){
+    this.height = parseFloat(event.target.value)
   }
 }
